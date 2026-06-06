9b0d0c38f85816d19e20e8449ebad0a13e4fd99c
2024it018-afk <2024it018@stu.vau.ac.lk>
2026-06-06 20:14:26 +0530

    Add files via upload

---
+++ test06.js
@@ -0,0 +1,35 @@
+/*const title=document.getElementById("title");
+const button=document.getElementById("changeBtn");
+button.addEventListener("click",function(){
+	title.textContent="button clicked!";
+	title.style.color="red";
+});*/
+/*const button=document.getElementById("colorBtn");
+button.addEventListener("click",function(){
+document.body.style.backgroundColor="lightblue";})*/
+
+/*function show(){
+	let name=document.getElementById("name").value;
+document.getElementById("result").innerText="hello "+name;}*/
+
+/*function buy(product){
+alert (product+"selected");}
+function great(){
+	alert("hello");
+}*/
+/*function hiddenImage(){
+	document.getElementById("box").style.display="none";
+}
+function show(){
+	document.getElementById("box").style.display="block";
+}*/
+function handle(file){
+	file.preventDefault();
+	let first name=document.getElementById("name").value;
+	let last name=document.getElementById("lname").value;
+	
+	console.log("first name:" +name);
+	console.log("last name:"+lname);
+	document.getElementById("otput").innerText="form success"+name;
+	document.getElementById("my").reset();
+	
